544ea8f6b1faed4440d6b5a0588e02fc221d90f9
Chu Nhi <chunhi.dev@gmail.com>
2025-07-22 22:35:13 +0700

    Modified Folder Tile & Add Game Booster: Hint & Shuffle

---
+++ Assets/_Game/Scripts/CoreGame/BoosterController.cs
@@ -0,0 +1,82 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class BoosterController : MonoBehaviour
+{
+    public void ShuffleButtonClick()
+    {
+        //TODO: SoundManager.Instance.PlayFx(FxID.Shuffle);
+        ShuffleTiles();
+    }
+    public void HintButtonClick()
+    {
+        //TODO: SoundManager.Instance.PlayFx(FxID.Hint);
+        FindHint();
+    }
+    private void FindHint()
+    {
+        Dictionary<TileType, List<Vector2Int>> tileDict = TileManager.Instance.TileTypeDic;
+        GameTile[,] tiles = TileManager.Instance.Tiles;
+        var keys = new List<TileType>(tileDict.Keys);
+        keys.Shuffle();
+        foreach(var key in keys) {
+            List<Vector2Int> hintPos = tileDict[key];
+            for (int i = 0; i < hintPos.Count; i++)
+            {
+                for (int j = i + 1; j < hintPos.Count; j++)
+                {
+                    Vector2Int posA = hintPos[i];
+                    Vector2Int posB = hintPos[j];
+                    
+                    if (TileManager.Instance.CheckPathExits(posA, posB))
+                    {
+                        TileManager.Instance.ForceMatchByBooster(tiles[posA.y, posA.x], tiles[posB.y, posB.x]);
+                        return;
+                    }
+                }
+            }
+        }
+        TileManager.Instance.CheckDeadLock();
+    }
+
+    private void ShuffleTiles()
+    {
+        GameTile[,] shuffledTiles = TileManager.Instance.Tiles;
+        List<TileType> activePos = new List<TileType>();
+        int width = shuffledTiles.GetLength(0);
+        int height = shuffledTiles.GetLength(1);
+
+        // Chọn ra những Tiles được Shuffle
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                if (shuffledTiles[i, j] != null 
+                    && (int)shuffledTiles[i, j].TileType < (int)TileType.Obstacles) // Không xào vật cản
+                {
+                    activePos.Add(shuffledTiles[i, j].TileType);
+                }
+            }
+        }
+        
+        activePos.Shuffle();
+        
+        //Update lại Tiles chính
+        int visitedIdx = 0;
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                if (shuffledTiles[i, j] != null 
+                    && (int)shuffledTiles[i, j].TileType < (int)TileType.Obstacles) // Không xào Vật cản
+                {
+                    shuffledTiles[i, j].SetCellType((int)activePos[visitedIdx++]);
+                }
+            }
+        }
+        
+        TileManager.Instance.SetTilesData(shuffledTiles);
+        TileManager.Instance.CheckDeadLock();
+    }
+}
